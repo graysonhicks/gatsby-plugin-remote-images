159ca37e159d0d28e30c13f848fb08a5caf050d6
Florian Bernard <ext.florian.bernard@king.com>
2020-05-02 13:41:10 +0200

    fix issues with path and cache

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -48,9 +48,9 @@ exports.onCreateNode = async (
   }
 };
 
-function getPaths(node, path, ext) {
+function getPaths(node, path, ext = null) {
   const value = get(node, path);
-  return value.map(path => getPath(node, path, ext));
+  return value.map(url => (ext ? url + ext : url));
 }
 
 // Returns value from path, adding extension when supplied
@@ -73,39 +73,46 @@ async function createImageNodes(urls, node, options) {
     return;
   }
 
-  urls.forEach(async (url, index) => {
-    if (typeof prepareUrl === 'function') {
-      url = prepareUrl({ url, node, index });
-    }
+  const fileNodes = urls
+    .map(async (url, index) => {
+      if (typeof prepareUrl === 'function') {
+        url = prepareUrl({
+          url,
+          node,
+          index,
+        });
+      }
 
-    try {
-      fileNode = await createRemoteFileNode({
-        ...restOfOptions,
-        url,
-        parentNodeId: node.id,
-      });
-    } catch (e) {
-      console.error('gatsby-plugin-remote-images ERROR:', e);
-    }
+      try {
+        fileNode = await createRemoteFileNode({
+          ...restOfOptions,
+          url,
+          parentNodeId: node.id,
+        });
+      } catch (e) {
+        console.error('gatsby-plugin-remote-images ERROR:', e);
+      }
+      return fileNode;
+    })
+    .filter(fileNode => !!fileNode);
 
-    // Store the mapping between the current node and the newly created File node
-    if (fileNode) {
-      // This associates the existing node (of user-specified type) with the new
-      // File nodes created via createRemoteFileNode. The new File nodes will be
-      // resolved dynamically through the Gatsby schema customization
-      // createResolvers API and which File node gets resolved for each new field
-      // on a given node of the user-specified type is determined by the contents
-      // of this mapping. The keys are based on the ID of the parent node (of
-      // user-specified type) and the values are each a nested mapping of the new
-      // image File field name to the ID of the new File node.
-      const cacheKey = getCacheKeyForNodeId(node.id);
-      const existingFileNodeMap = await options.cache.get(cacheKey);
-      await options.cache.set(cacheKey, {
-        ...existingFileNodeMap,
-        [name]: [...existingFileNodeMap[name], fileNode.id],
-      });
-    }
-  });
+  // Store the mapping between the current node and the newly created File node
+  if (fileNodes.length) {
+    // This associates the existing node (of user-specified type) with the new
+    // File nodes created via createRemoteFileNode. The new File nodes will be
+    // resolved dynamically through the Gatsby schema customization
+    // createResolvers API and which File node gets resolved for each new field
+    // on a given node of the user-specified type is determined by the contents
+    // of this mapping. The keys are based on the ID of the parent node (of
+    // user-specified type) and the values are each a nested mapping of the new
+    // image File field name to the ID of the new File node.
+    const cacheKey = getCacheKeyForNodeId(node.id);
+    const existingFileNodeMap = await options.cache.get(cacheKey);
+    await options.cache.set(cacheKey, {
+      ...existingFileNodeMap,
+      [name]: fileNodes.map(({ id }) => id),
+    });
+  }
 }
 
 // Creates a file node and associates the parent node to its new child

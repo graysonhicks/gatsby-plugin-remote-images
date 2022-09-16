44701e710eba7742861a85c20c5dd9bd8397b512
Grayson Hicks <graysonhicks@gmail.com>
2022-09-16 13:55:42 -0500

    jest config

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+const config = {
+  moduleNameMapper: {
+    '^gatsby-page-utils/(.*)$': `gatsby-page-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
+    '^gatsby-core-utils/(.*)$': `gatsby-core-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
+    '^gatsby-plugin-utils/(.*)$': [
+      `gatsby-plugin-utils/dist/$1`,
+      `gatsby-plugin-utils/$1`,
+    ], // Workaround for https://github.com/facebook/jest/issues/9771
+  },
+};
+
+module.exports = config;
